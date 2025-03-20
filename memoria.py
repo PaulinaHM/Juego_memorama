a47660c915ccfde3e4f2bec41bb89aeeeab7b0e8
PaulinaHM <paulinahernandez318@aragon.unam.mx>
2025-03-20 00:36:15 -0600

    Script con comentarios

--- memoria.py
+++ memoria.py
@@ -6,68 +6,90 @@ import time
 import random
 
 
+#Iniciamos todo lo de Pygame para poder usar sonido, pantalla, imagenes y efectos
+
 pygame.init()
 pygame.font.init()
 pygame.mixer.init()
 
 
+#Variables y configuraciones que vamos a usar a lo largo del programa
+altura_boton = 30  # Del botón de abajo, para iniciar juego
+medida_cuadro = 160  # Medida de la imagen en pixeles
 
-altura_boton = 30 
-medida_cuadro = 160  
-nombre_imagen_oculta = "assets/oculta.png"
+# Lo siguiente configura la parte trasera de cada tarjeta
+nombre_imagen_oculta = "archivos/oculta.png"
 imagen_oculta = pygame.image.load(nombre_imagen_oculta)
-segundos_mostrar_pieza = 2  
+segundos_mostrar_pieza = 2  # Segundos para ocultar la pieza si no es la correcta
 
 
 class Cuadro:
     def __init__(self, fuente_imagen):
         self.mostrar = True
         self.descubierto = False
-
         self.fuente_imagen = fuente_imagen
         self.imagen_real = pygame.image.load(fuente_imagen)
 
+
+#Toda la configuración del juego que al final se toma como un arreglo de objetos
+
 cuadros = [
-    [Cuadro("assets/coco.png"), Cuadro("assets/coco.png"),
-     Cuadro("assets/manzana.png"), Cuadro("assets/manzana.png")],
-    [Cuadro("assets/limón.png"), Cuadro("assets/limón.png"),
-     Cuadro("assets/naranja.png"), Cuadro("assets/naranja.png")],
-    [Cuadro("assets/pera.png"), Cuadro("assets/pera.png"),
-     Cuadro("assets/piña.png"), Cuadro("assets/piña.png")],
-    [Cuadro("assets/plátano.png"), Cuadro("assets/plátano.png"),
-     Cuadro("assets/sandía.png"), Cuadro("assets/sandía.png")],
+    [Cuadro("archivos/coco.png"), Cuadro("archivos/coco.png"),
+     Cuadro("archivos/manzana.png"), Cuadro("archivos/manzana.png")],
+    [Cuadro("archivos/limón.png"), Cuadro("archivos/limón.png"),
+     Cuadro("archivos/naranja.png"), Cuadro("archivos/naranja.png")],
+    [Cuadro("archivos/pera.png"), Cuadro("archivos/pera.png"),
+     Cuadro("archivos/piña.png"), Cuadro("archivos/piña.png")],
+    [Cuadro("archivos/plátano.png"), Cuadro("archivos/plátano.png"),
+     Cuadro("archivos/sandía.png"), Cuadro("archivos/sandía.png")],
 ]
+
+# Colores
 color_blanco = (255, 255, 255)
 color_negro = (0, 0, 0)
 color_gris = (206, 206, 206)
 color_azul = (30, 136, 229)
 
-sonido_fondo = pygame.mixer.Sound("assets/fondo.wav")
-sonido_clic = pygame.mixer.Sound("assets/clic.wav")
-sonido_exito = pygame.mixer.Sound("assets/ganador.wav")
-sonido_fracaso = pygame.mixer.Sound("assets/equivocado.wav")
-sonido_voltear = pygame.mixer.Sound("assets/voltear.wav")
+# Los sonidos
+sonido_fondo = pygame.mixer.Sound("archivos/fondo.wav")
+sonido_clic = pygame.mixer.Sound("archivos/clic.wav")
+sonido_exito = pygame.mixer.Sound("archivos/ganador.wav")
+sonido_fracaso = pygame.mixer.Sound("archivos/equivocado.wav")
+sonido_voltear = pygame.mixer.Sound("archivos/voltear.wav")
 
+# Calculamos el tamaño de la pantalla en base al tamaño de los cuadrados
 anchura_pantalla = len(cuadros[0]) * medida_cuadro
 altura_pantalla = (len(cuadros) * medida_cuadro) + altura_boton
 anchura_boton = anchura_pantalla
 
+# La fuente que estará sobre el botón
 tamanio_fuente = 20
 fuente = pygame.font.SysFont("Arial", tamanio_fuente)
 xFuente = int((anchura_boton / 2) - (tamanio_fuente / 2))
 yFuente = int(altura_pantalla - altura_boton)
 
+# El botón, que al final es un rectángulo
 boton = pygame.Rect(0, altura_pantalla - altura_boton,
                     anchura_boton, altura_pantalla)
 
+# Bandera para saber si se debe ocultar la tarjeta dentro de N segundos
 ultimos_segundos = None
-puede_jugar = True 
+puede_jugar = True  # Bandera para saber si reaccionar a los eventos del usuario
+
+# Saber si el juego está iniciado; así sabemos si ocultar o mostrar piezas, además del botón
 juego_iniciado = False
+
+# Banderas de las tarjetas cuando se busca una pareja. Las necesitamos como índices para el arreglo de cuadros
+# x1 con y1 sirven para la primer tarjeta
 x1 = None
 y1 = None
+
+# Y las siguientes para la segunda tarjeta
 x2 = None
 y2 = None
 
+
+# Ocultar todos los cuadros al inicio del juego
 def ocultar_todos_los_cuadros():
     for fila in cuadros:
         for cuadro in fila:
@@ -76,6 +98,7 @@ def ocultar_todos_los_cuadros():
 
 
 def aleatorizar_cuadros():
+    # Elegir X e Y aleatorios
     cantidad_filas = len(cuadros)
     cantidad_columnas = len(cuadros[0])
     for y in range(cantidad_filas):
@@ -92,6 +115,9 @@ def comprobar_si_gana():
         pygame.mixer.Sound.play(sonido_exito)
         reiniciar_juego()
 
+
+# Regresa False si al menos un cuadro NO está descubierto.
+# True en caso de que absolutamente todos estén descubiertos
 def gana():
     for fila in cuadros:
         for cuadro in fila:
@@ -115,41 +141,55 @@ def iniciar_juego():
     juego_iniciado = True
 
 
+
+#Iniciamos la pantalla con las medidas previamente calculadas, colocamos título y reproducimos el sonido de fondo
+
 pantalla_juego = pygame.display.set_mode((anchura_pantalla, altura_pantalla))
 pygame.display.set_caption('Memorama en Python')
-
+pygame.mixer.Sound.play(sonido_fondo, -1)  # El -1 indica un loop infinito
+# Ciclo infinito...
 while True:
- 
+    # Escuchar eventos, pues estamos en un ciclo infinito que se repite varias veces por segundo
     for event in pygame.event.get():
+        # Si quitan el juego, salimos
         if event.type == pygame.QUIT:
             sys.exit()
+        # cuando detecta el clic, el usuario puede jugar...
         elif event.type == pygame.MOUSEBUTTONDOWN and puede_jugar:
 
+            # Si el click fue sobre el botón y el juego no se ha iniciado, entonces iniciamos el juego
             xAbsoluto, yAbsoluto = event.pos
             if boton.collidepoint(event.pos):
                 if not juego_iniciado:
                     iniciar_juego()
 
             else:
+                # Si no hay juego iniciado, ignoramos el clic
                 if not juego_iniciado:
                     continue
-              
+                
                 x = math.floor(xAbsoluto / medida_cuadro)
                 y = math.floor(yAbsoluto / medida_cuadro)
+                # Si  ya está mostrada o descubierta, no hacemos nada
                 cuadro = cuadros[y][x]
                 if cuadro.mostrar or cuadro.descubierto:
+                    # continue ignora lo de abajo y deja que el ciclo siga
                     continue
+                # Si es la primera vez que tocan la imagen (es decir, no están buscando el par de otra, sino apenas están descubriendo la primera)
                 if x1 is None and y1 is None:
+                    # Entonces la actual es en la que acaban de dar clic, la mostramos
                     x1 = x
                     y1 = y
                     cuadros[y1][x1].mostrar = True
                     pygame.mixer.Sound.play(sonido_voltear)
                 else:
+                    # En caso de que ya hubiera una clickeada anteriormente y estemos buscando el par, comparamos...
                     x2 = x
                     y2 = y
                     cuadros[y2][x2].mostrar = True
                     cuadro1 = cuadros[y1][x1]
                     cuadro2 = cuadros[y2][x2]
+                    # Si coinciden, entonces a ambas las ponemos en descubiertas:
                     if cuadro1.fuente_imagen == cuadro2.fuente_imagen:
                         cuadros[y1][x1].descubierto = True
                         cuadros[y2][x2].descubierto = True
@@ -160,11 +200,18 @@ while True:
                         pygame.mixer.Sound.play(sonido_clic)
                     else:
                         pygame.mixer.Sound.play(sonido_fracaso)
+                        # Si no coinciden, tenemos que ocultarlas en el plazo de [segundos_mostrar_pieza] segundo(s). Así que establecemos
+                        # la bandera. Como esto es un ciclo infinito y asíncrono, podemos usar el tiempo para saber
+                        # cuándo fue el tiempo en el que se empezó a ocultar
                         ultimos_segundos = int(time.time())
+                        # Hasta que el tiempo se cumpla, el usuario no puede jugar
                         puede_jugar = False
                 comprobar_si_gana()
 
     ahora = int(time.time())
+    # Y aquí usamos la bandera del tiempo, de nuevo. Si los segundos actuales menos los segundos
+    # en los que se empezó el ocultamiento son mayores a los segundos en los que se muestra la pieza, entonces
+    # se ocultan las dos tarjetas y se reinician las banderas
     if ultimos_segundos is not None and ahora - ultimos_segundos >= segundos_mostrar_pieza:
         cuadros[y1][x1].mostrar = False
         cuadros[y2][x2].mostrar = False
@@ -173,11 +220,16 @@ while True:
         x2 = None
         y2 = None
         ultimos_segundos = None
+        # En este momento el usuario ya puede hacer clic de nuevo pues las imágenes ya estarán ocultas
         puede_jugar = True
 
+    # Hacer toda la pantalla blanca
     pantalla_juego.fill(color_blanco)
+    # Banderas para saber en dónde dibujar las imágenes, pues al final
+    # la pantalla de PyGame son solo un montón de pixeles
     x = 0
     y = 0
+    # Recorrer los cuadros
     for fila in cuadros:
         x = 0
         for cuadro in fila:
@@ -192,7 +244,9 @@ while True:
             x += medida_cuadro
         y += medida_cuadro
 
+    # También dibujamos el botón
     if juego_iniciado:
+        # Si está iniciado, entonces botón blanco con fuente gris para que parezca deshabilitado
         pygame.draw.rect(pantalla_juego, color_blanco, boton)
         pantalla_juego.blit(fuente.render(
             "Iniciar juego", True, color_gris), (xFuente, yFuente))
@@ -201,4 +255,5 @@ while True:
         pantalla_juego.blit(fuente.render(
             "Iniciar juego", True, color_blanco), (xFuente, yFuente))
 
+    # Actualizamos la pantalla
     pygame.display.update()
